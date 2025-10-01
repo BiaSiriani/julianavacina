3ee9c8ab590f0b9d8cdecf8fa3c9939314c580d2
biasiriani <biasiriani200722@gmail.com>
2025-10-01 14:35:46 -0300

    llo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Topbar from "./component/topbar";
-import Campanhas from "./component/Campanhas";
+import Campanhas from "./component/campanhas";
 export default function Home() {
   return (
    <>
